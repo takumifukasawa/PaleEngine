cc0d80e55dcfeff28a9dd8b45626741fc5758b65
takumifukasawa <takumifukasawa@gmail.com>
2025-04-11 00:53:37 +0900

    subtree

---
+++ subtree-pale-gl.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+
+repo=https://github.com/takumifukasawa/PaleGL.git
+remote=origin-pale-gl
+branch=master
+path=PaleGL
+
+if [ $# -eq 0 ]; then
+	echo "this is git-subtree-suppot tool.\noptions:\n init\n pull\n push"
+	exit 1
+fi
+
+if [ $1 = init ]; then
+	git remote add ${remote} ${repo}
+	git fetch ${remote}
+	git subtree add --prefix=${path} ${remote} ${branch}
+elif [ $1 = pull ] ; then
+	git subtree pull --prefix=${path} ${remote} ${branch}
+elif [ $1 = push ] ; then
+	git subtree push --prefix=${path} ${remote} ${branch}
+else
+	echo "$1 option not found"
+fi
